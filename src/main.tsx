1f5f1e63b7560992eb87bc87d63916f6419a21d7
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-09-29 18:17:40 -0300

    feat: add theme provider from styled-components

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { GlobalStyle } from "@/styles/globalStyles.ts";
+import { Theme } from "./styles/Theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <GlobalStyle />
+    <Theme>
+      <App />
+      <GlobalStyle />
+    </Theme>
   </React.StrictMode>
 );
