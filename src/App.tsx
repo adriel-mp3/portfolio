b194f237ef41d687c4d79bf8308634dbc181b953
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-01 16:41:29 -0300

    feat: import home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Header from "@/layout/Header";
+import Home from "@/pages/Home";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
