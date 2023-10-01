e63c853ef17260543d47e4d177ac8514fb041a36
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-01 16:43:09 -0300

    feat: create tag reusable component

---
+++ src/components/Tag/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Tag = styled.span`
+  display: block;
+  width: fit-content;
+  border-radius: 1.25rem;
+  padding: 0.38rem 0.75rem;
+  background: ${({ theme }) => theme.colors["accent-100"]};
+  color: ${({ theme }) => theme.colors["accent-500"]};
+  font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+`;
