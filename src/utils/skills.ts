f026a3da6a5d7fd5ecaa8ecc7649d7d2b52ea705
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-01 16:41:03 -0300

    feat: create skills array

---
+++ src/utils/skills.ts
@@ -0,0 +1,20 @@
+const skills = [
+  "React",
+  "TypeScript",
+  "Next.JS",
+  "GraphQL",
+  "Apollo",
+  "Styled Components",
+  "Tailwind",
+  "SASS",
+  "UI/UX",
+  "Git",
+  "Figma",
+  "GitHub",
+  "JavaScript",
+  "Storybook",
+  "Jest",
+  "React Testing Library",
+] as const;
+
+export default skills;
