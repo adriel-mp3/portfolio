9d6c73c49d94577c4f90eebb9f51da90ceedeb7f
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-12 16:22:30 -0300

    chore: create my social networks alias

---
+++ src/utils/socialNetworks.ts
@@ -0,0 +1,12 @@
+const socialNetworks = {
+  github: {
+    link: "https://github.com/adriel-mp3",
+  },
+  linkedin: {
+    link: "https://www.linkedin.com/in/adriel-santos-dev/",
+  },
+  email: {
+    address: "contatoadrielsantos@hotmail.com",
+  },
+} as const;
+export default socialNetworks;
