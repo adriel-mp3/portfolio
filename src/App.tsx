28a14acfca245d18c776557a99358e2d80fd4686
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-12 16:22:56 -0300

    feat: add footer to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "@/layout/Header";
 import Home from "@/pages/Home";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <Footer/>
     </div>
   );
 }
