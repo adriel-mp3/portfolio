2e1d11223750d427012a524d84b01e9beeb18144
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-12 16:20:32 -0300

    feat: import Projects section to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Projects />
     </div>
   );
 };
