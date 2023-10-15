a19346e2904ecd699c1d9cd7589ca72c1b806205
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-15 17:51:12 -0300

    feat: create social networks util

--- src/utils/socialNetworks.ts
+++ src/utils/socialNetworks.ts
@@ -1,12 +1,37 @@
-const socialNetworks = {
-  github: {
+import githubIconLight from "@/assets/github.svg";
+import linkedinIconLight from "@/assets/linkedin.svg";
+import inboxIconLight from "@/assets/inbox.svg";
+import linkedinIconDark from "@/assets/linkedin-dark.svg";
+import githubIconDark from "@/assets/github-dark.svg";
+import inboxIconDark from "@/assets/inbox-dark.svg";
+
+const socialNetworks = [
+  {
+    name: "github",
     link: "https://github.com/adriel-mp3",
+    icon: {
+      dark: githubIconDark,
+      light: githubIconLight,
+    },
+    alt: "Github Icon",
   },
-  linkedin: {
+  {
+    name: "linkedin",
     link: "https://www.linkedin.com/in/adriel-santos-dev/",
+    icon: {
+      dark: linkedinIconDark,
+      light: linkedinIconLight,
+    },
+    alt: "Linkedin Icon",
   },
-  email: {
+  {
+    name: "email",
     address: "contatoadrielsantos@hotmail.com",
+    icon: {
+      dark: inboxIconDark,
+      light: inboxIconLight,
+    },
+    alt: "Email Icon",
   },
-} as const;
+];
 export default socialNetworks;
