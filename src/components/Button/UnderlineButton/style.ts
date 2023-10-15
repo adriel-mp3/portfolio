23803e429c3d2518721bbe602e48a9957126b5cd
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-15 16:11:13 -0300

    feat: create UnderlineButton

--- src/components/Button/UnderlineButton/style.ts
+++ src/components/Button/UnderlineButton/style.ts
@@ -1,26 +1,41 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const Button = styled.button`
+type ButtonSizes = "small" | "large";
+
+type UnderlineButton = {
+  $size?: ButtonSizes;
+};
+
+const BUTTON_SIZES = {
+  default: css`
+    font-size: ${({ theme }) => theme.fontSizes.base};
+    padding-bottom: 4px;
+    border-width: 2px;
+  `,
+  small: css`
+    font-size: ${({ theme }) => theme.fontSizes.base};
+    padding-bottom: 4px;
+    border-width: 2px;
+  `,
+  large: css`
+    font-size: ${({ theme }) => theme.fontSizes.xl};
+    font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+    padding-bottom: 8px;
+    border-width: 6px;
+  `,
+};
+
+export const UnderlineButton = styled.button<UnderlineButton>`
+  ${(props) =>
+    props.$size ? BUTTON_SIZES[props.$size] : BUTTON_SIZES["default"]}
+
   color: ${({ theme }) => theme.colors["neutral-300"]};
   border-bottom-style: solid;
   border-color: ${({ theme }) => theme.colors["neutral-300"]};
   transition: 0.3s;
-  
+
   &:hover {
     color: ${({ theme }) => theme.colors["accent-500"]};
     border-color: ${({ theme }) => theme.colors["accent-500"]};
   }
-`;
-
-export const ButtonLarge = styled(Button)`
-  font-size: ${({ theme }) => theme.fontSizes.xl};
-  font-weight: ${({ theme }) => theme.fontWeights.semiBold};
-  padding-bottom: 8px;
-  border-width: 6px;
-`;
-
-export const ButtonSmall = styled(Button)`
-  font-size: ${({ theme }) => theme.fontSizes.base};
-  padding-bottom: 4px;
-  border-width: 2px;
 `;
