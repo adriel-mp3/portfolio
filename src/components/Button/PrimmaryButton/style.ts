6fdf4fd90f29fd152d847126e62607f3bef0a4f1
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-10-15 14:08:10 -0300

    feat: create PrimmaryButton

--- src/components/Button/PrimmaryButton/style.ts
+++ src/components/Button/PrimmaryButton/style.ts
@@ -1,6 +1,32 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const Button = styled.button`
+type ButtonVariants = "dark" | "light";
+
+export type PrimaryButtonProps = {
+  $variant?: ButtonVariants;
+};
+
+const BUTTON_VARIANTS = {
+  default: css`
+    background: ${({ theme }) => theme.colors["accent-100"]};
+    color: ${({ theme }) => theme.colors["neutral-400"]};
+  `,
+  light: css`
+    background: ${({ theme }) => theme.colors["accent-100"]};
+    color: ${({ theme }) => theme.colors["neutral-400"]};
+  `,
+  dark: css`
+    background: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors["neutral-400"]};
+  `,
+};
+
+export const PrimaryButton = styled.button<PrimaryButtonProps>`
+  ${(props) =>
+    props.$variant
+      ? BUTTON_VARIANTS[props.$variant]
+      : BUTTON_VARIANTS["default"]}
+
   display: block;
   max-width: fit-content;
   padding: 12px 24px;
@@ -11,16 +37,6 @@ const Button = styled.button`
   letter-spacing: 0.05rem;
   transition: 0.3s;
   cursor: pointer;
-`;
-
-export const ButtonDark = styled(Button)`
-  background: ${({ theme }) => theme.colors.background};
-  color: ${({ theme }) => theme.colors["neutral-400"]};
-`;
-
-export const ButtonLight = styled(Button)`
-  background: ${({ theme }) => theme.colors["accent-100"]};
-  color: ${({ theme }) => theme.colors["neutral-400"]};
 
   &:hover {
     filter: brightness(120%);
