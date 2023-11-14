2753f62e2d764b4ceede980600a798d43fdfe233
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-11-14 12:42:12 -0300

    feat: add description for projects

--- src/utils/projectsInfo.ts
+++ src/utils/projectsInfo.ts
@@ -8,35 +8,35 @@ export type ProjectsInfoType = {
 
 export const projectsInfo = [
   {
-    name: "Revive Mart ",
+    name: "Revive Mart",
     description:
-      'No contexto de software de código aberto, por conta da sua natureza dinâmica e participativa por meio da Internet, a palavra "projeto" geralmente está ligada ao próprio software.',
-    tags: ["Styled Components", "React", "TypeScript", "Figma", "UI/UX"],
+      "Uma plataforma de e-commerce focada em produtos recondicionados. Nela, você pode ter acesso a todos os produtos, divididos por categorias.",
+    tags: ["Styled Components", "React", "TypeScript", "API REST", "Figma"],
     repository: "https://github.com/adriel-mp3/revive-mart",
     deploy: "https://revive-mart.vercel.app",
   },
   {
     name: "Events",
     description:
-      'No contexto de software de código aberto, por conta da sua natureza dinâmica e participativa por meio da Internet, a palavra "projeto" geralmente está ligada ao próprio software.',
-    tags: ["Tailwind", "React", "TypeScript", "JavaScript"],
+      "Você pode ter acesso a palestras e eventos em todos os dispositivos, além de poder comprar ingressos. A plataforma está disponível para todas as plataformas.",
+    tags: ["Tailwind", "React", "JavaScript", "React Router"],
     repository: "https://github.com/adriel-mp3/events",
     deploy: "",
   },
   {
-    name: "Events",
+    name: "In-Order",
     description:
-      'No contexto de software de código aberto, por conta da sua natureza dinâmica e participativa por meio da Internet, a palavra "projeto" geralmente está ligada ao próprio software.',
-    tags: ["Tailwind", "React", "TypeScript", "JavaScript", "JavaScript"],
-    repository: "https://github.com/adriel-mp3/events",
-    deploy: "",
-  },
-  {
-    name: "Events",
-    description:
-      'No contexto de software de código aberto, por conta da sua natureza dinâmica e participativa por meio da Internet, a palavra "projeto" geralmente está ligada ao próprio software.',
+      "Gerenciador de tarefas simples onde você pode organizar e otimizar facilmente sua rotina. Pode adicionar ou remover tarefas, definir prioridades e entre outras funcionalidades.",
     tags: ["Tailwind", "React", "TypeScript", "JavaScript"],
     repository: "https://github.com/adriel-mp3/events",
+    deploy: "https://adriel-mp3.github.io/in-order/",
+  },
+  {
+    name: "Meu Portfólio",
+    description:
+      "O objetivo deste é apresentar as principais informações da minha carreira como desenvolvedor de forma simples e didática. Está disponível para todos os dispositivos.",
+    tags: ["Styled Components", "React", "TypeScript", "Figma"],
+    repository: "https://github.com/adriel-mp3/portfolio",
     deploy: "",
   },
 ] as ProjectsInfoType[];
