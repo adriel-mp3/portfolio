c93daf5fb1ee8194a5d95c71c02b564ac83707c6
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-11-14 12:41:50 -0300

    feat: add id for headerLinks

--- src/utils/headerLinks.ts
+++ src/utils/headerLinks.ts
@@ -1,18 +1,22 @@
 export type HeaderLinksType = {
+  id: string;
   href: string;
   label: string;
 };
 
 export const headerLinks = [
   {
+    id: "sobre",
     href: "#about",
     label: "Sobre",
   },
   {
+    id: "projects",
     href: "#projects",
     label: "Projetos",
   },
   {
+    id: "contact",
     href: "#contact",
     label: "Contato",
   },
